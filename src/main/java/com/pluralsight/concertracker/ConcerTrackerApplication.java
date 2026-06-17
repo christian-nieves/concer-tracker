cc89dc9e8abad7bd6bb795816ab6a34e78c27bfc
christian-nieves <thechristiannieves@gmail.com>
2026-06-17 11:23:06 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/concertracker/ConcerTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.concertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcerTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcerTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pluralsight/concertracker/ConcerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.concertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcerTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
